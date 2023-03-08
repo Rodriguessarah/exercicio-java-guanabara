4a30e6ac5acf1d1004b98a7905c75f6e7dda61d7
Sarah <Sarah@LAPTOP-36O8HRC6>
2023-03-07 21:12:59 -0300

    exercicios

---
+++ exercicios/src/exercicios/exercicio06.java
@@ -0,0 +1,24 @@
+package exercicios;
+import java.util.Scanner;
+
+public class exercicio06 {
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner (System.in);	
+		
+		int num, antec, suces;
+		
+		System.out.println("informe um numero inteiro");
+		num = read.nextInt();
+		
+		antec = num-1;
+		System.out.println(" o antecessor de " + num + "é " + antec);
+		
+		
+		suces = num +1;
+		System.out.println(" o sucessor de " + num + "é " + suces);
+		
+	
+	}
+
+}
