87072a1fe01a62d59b34e5b65badbf7d35bac5bf
Rodriguessarah <Sarah@LAPTOP-36O8HRC6>
2023-03-09 21:47:46 -0300

    Resolução de exercicios

--- exercicios/src/exercicios/exercicio09.java
+++ exercicios/src/exercicios/exercicio09.java
@@ -1,13 +1,22 @@
 package exercicios;
+
 import java.util.Scanner;
 
 public class exercicio09 {
 
 	public static void main(String[] args) {
 		
-		Scanner read = new Scanner (System.in); 
+		double reais;
+		float dolar;
+		Scanner read = new Scanner (System.in);
 		
-
+		System.out.println("informe o valor em reais");
+		reais = read.nextDouble();
+		//US$1,00 = R$5,17 //
+		
+		dolar =  (float) (reais/5.17);
+		System.out.printf(" o valor em dolares é de US$ %.2f", dolar);
+		
 	}
 
 }
