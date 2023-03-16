5d63fb25360b6a7b4f4b7851dcc10e8d55c288fd
Rodriguessarah <Sarah@LAPTOP-36O8HRC6>
2023-03-15 21:51:21 -0300

    Resolução de Exercicios

---
+++ exercicios/src/exercicios/exercicio14.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+public class exercicio14 {
+	public static void main(String[] args) {
+		
+		double kmPercorrido, diasDeAluguel, precoTotal;
+		
+		Scanner read = new Scanner(System.in);
+		System.out.println("Informe a quantidade de KM percorrido:");
+		kmPercorrido = read.nextDouble();
+		
+		System.out.println("Informque a quantidade de dias de aluguel");
+		diasDeAluguel = read.nextDouble();
+		
+		precoTotal =(kmPercorrido*0.20)+ (diasDeAluguel*90);
+		System.out.println(" o valor  total do aluguel é " + precoTotal + "reais");
+}
+
+}
